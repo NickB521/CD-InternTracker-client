62e001740a408e63dfa479ae7d676eeb74d9d5cc
folashayok <folashayokehinde11@gmail.com>
2025-02-20 16:52:28 -0500

    checkout app.css and app.jsx files from dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Container } from '@mui/material';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Performance from './components/Performance';
+import BarsDataset from './components/BarsData';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container maxWidth="md">
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<div>Login Content</div>} />
+          <Route path="/dashboard/:id" element={<div>Dashboard Content</div>} />
+          <Route path="/dashboard/:id/management" element={<div>Management Content</div>} />
+          <Route path="/dashboard/:id/attendance" element={<div>Attendance Content</div>} />
+          <Route path="/dashboard/:id/performance" element={<Performance />} />
+          <Route path="/dashboard/:id/studentcharts" element={<BarsDataset />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
 }
 
-export default App
+export default App;
